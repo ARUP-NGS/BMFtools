8a909092c29996f3d06d5309aea727e2be46c5bd
Daniel Baker <daniel.baker@aruplab.com>
2014-07-16 16:30:01 -0600

    Extended JSON parser.

--- parseJson.sh
+++ parseJson.sh
@@ -2,5 +2,5 @@
 FILENAME=$1
 echo $FILENAME:Count > ${FILENAME%.*}.xTractCount
 echo $FILENAME:Fraction  > ${FILENAME%.*}.xTractFrac 
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'count' >> ${FILENAME%.*}.xTractCount
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'fraction' >> ${FILENAME%.*}.xTractFrac
+cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'count' | awk 'BEGIN {FS="\t";OFS="\t"};{print $NF}' >> ${FILENAME%.*}.xTractCount
+cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'fraction' | awk 'BEGIN {FS="\t";OFS="\t"};{print $NF}' >> ${FILENAME%.*}.xTractFrac
