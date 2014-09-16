20d78e9626b155d52fd056bfcb1fb435aa859cc6
Daniel Baker <daniel.baker@aruplab.com>
2014-09-16 11:27:26 -0600

    Prepared for ARUP-NGS.

--- InProgressOrOld/Wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-Full=$1
-N30=$2
-
-/mounts/Project_Sarcoma_Lib_709/BamUtils.sh $1
-/mounts/Project_Sarcoma_Lib_709/BamUtils.sh $2
-/mounts/Project_Sarcoma_Lib_709/MergeDelly.sh $1
--- Utilities/parseJson.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILENAME=$1
-echo $FILENAME:Count > ${FILENAME%.*}.xTractCount
-echo $FILENAME:Fraction  > ${FILENAME%.*}.xTractFrac 
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'count' | awk 'BEGIN {FS="\t";OFS="\t"};{print $NF}' >> ${FILENAME%.*}.xTractCount
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'fraction' | awk 'BEGIN {FS="\t";OFS="\t"};{print $NF}' >> ${FILENAME%.*}.xTractFrac
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'count' | awk 'BEGIN {FS="\t";OFS="\t"};{print $1}' >> ${FILENAME%.*}.xTractCountLabels
-cat $FILENAME | tr ',' '\n' | sed 's:{:{\n:g' | sed 's:"\|{\|}::g' | sed 's/:/\t/g' | grep 'fraction' | awk 'BEGIN {FS="\t";OFS="\t"};{print $1}' >> ${FILENAME%.*}.xTractFracLabels
-
