f7808d32038136d0f051def6994bb3f21464c48d
Daniel Baker <daniel.baker@aruplab.com>
2015-01-06 16:43:56 -0700

    Created an installation script which will install both the python packages and the standalone executable.

---
+++ INSTALL.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from subprocess import *
+import argparse
+
+
+def main():
+    check_call(["python", "setup.py", "install"])
+    except CalledProcessError:
+        print("You might not have permission to install the BMFTools packages.")
+        return 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--prefix',
+        help=("Will attempt to copy"
+        " the main executable to the folder provided."
+        " Defaults to /usr/local/bin"),
+        default="/usr/local/bin"
+        )
+    args = parser.parse_args()
+    try:
+        check_call(["cp", "BMFMain/main.py", args.prefix + "/bmftools"])
+    except CalledProcessError:
+        print("You might not have permission to install the bmftools executable.")
+        return 1
+    return
+
+
+if(__name__ == "__main__"):
+    main()
