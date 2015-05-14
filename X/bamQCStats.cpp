ce3f93a7df21e2342f1a5fe52d18679783244004
dnephi <daniel.baker@aruplab.com>
2015-05-14 11:32:49 -0600

    Playing around.

--- X/bamQCStats.cpp
+++ X/bamQCStats.cpp
@@ -3,24 +3,27 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
+#include <numeric>
 #include "api/BamReader.h"
 #include "api/BamWriter.h"
 
-using namespace std;
 using namespace BamTools;
 
 int main(int argc, char* argv[])
 {
     //MaxInsert is the largest insert included in the calculation of mean insert size.
     const long MaxInsert = 2000;
-    string inputFilename;
+    std::string inputFilename;
+    std::vector<float> AFs;
+    std::vector<float> InsertVec;
     float meanAF, AF, AFSum, fracMapped, meanInsert;
     long numReads, numMapped, InsertSum, NonzeroInserts;
     if(argc == 2) {
         inputFilename = argv[1];
     }
     else if(argc > 2) {
-        cerr << "Too many arguments" << endl;
+        std::cerr << "Too many arguments" << std::endl;
         return 1;
     }
     else {
@@ -28,35 +31,39 @@ int main(int argc, char* argv[])
         printf("Usage: \n");
         printf("First positional argument: input BAM\n");
         printf("That is all.\n");
+        return 1;
     }
     BamReader reader;
     if(!reader.Open(inputFilename)) {
-        cerr << "Could not open input BAM" << endl;
+        std::cerr << "Could not open input BAM" << std::endl;
         return 1;
     }
     const SamHeader header = reader.GetHeader();
     const RefVector references = reader.GetReferenceData();
     BamAlignment rec;
-    AFSum = 0.;
-    numReads = numMapped = NonzeroInserts = InsertSum = 0;
+    numReads = NonzeroInserts = InsertSum = 0;
 
     while (reader.GetNextAlignment(rec)) {
         numReads++;
         if(rec.IsMapped()) {
             rec.GetTag("AF", AF);
-            numMapped++;
-            AFSum += AF;
+            AFs.push_back(AF);
             if(rec.InsertSize != 0 && abs(rec.InsertSize) < MaxInsert) {
                 NonzeroInserts++;
-                InsertSum += abs(rec.InsertSize);
+                InsertVec.push_back(abs(rec.InsertSize));
             }
         }
 
     }
+    NonzeroInserts = long(InsertVec.size());
+    AFSum = std::accumulate(AFs.begin(), AFs.end(), 0.0);
+    InsertSum = std::accumulate(InsertVec.begin(), InsertVec.end(), 0);
+    numMapped = long(AFs.size());
     meanAF = AFSum / numMapped;
     fracMapped = float(numMapped) / numReads;
     meanInsert = float(InsertSum) / NonzeroInserts;
-    printf("Mean Aligned Fraction %s=%f\n", inputFilename.c_str(), meanAF);
+    printf("#Filename=%s\n", inputFilename.c_str());
+    printf("Mean Aligned Fraction=%f\n", meanAF);
     printf("Reads mapped=%i\n", numMapped);
     printf("Reads total=%i\n", numReads);
     printf("Fraction aligned=%f\n", fracMapped);
