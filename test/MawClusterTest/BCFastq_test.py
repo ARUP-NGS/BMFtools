e824d851b776b61ea445b0f2749fc449eaad5124
BrettKennedy <brett.jacob.kennedy@gmail.com>
2015-06-09 11:51:15 -0600

    cleaned up and fixed some unit testing.

--- test/MawClusterTest/BCFastq_test.py
+++ test/MawClusterTest/BCFastq_test.py
@@ -2,6 +2,7 @@ __author__ = 'dnephi'
 
 import unittest
 import sys
+import subprocess
 from utilBMF.HTSUtils import pFastqFile
 from MawCluster.BCFastq import (cFRF_helper as compareFqRecsFast,
                                 pairedFastqConsolidate, singleFastqConsolidate)
@@ -12,7 +13,7 @@ class MyTestCase(unittest.TestCase):
         pass
 
     def setUp(self):
-        self.handle = pFastqFile("TestR1.fastq")
+        self.handle = pFastqFile("../data/TestR1.fastq")
         self.prefastqs = [i for i in self.handle]
         #self.handle.refresh()
         #create dummy ucsc references try
@@ -26,9 +27,11 @@ class MyTestCase(unittest.TestCase):
         pass
 
     def test_sfc(self):
-        singleFastqConsolidate("TestR1.fastq")
-        conFq = open("TestR1.cons.fastq").readlines()
-        assert conFq == ['@AAAAAATGGACCCATTAACC 1:N:0:ACAGTG|FP=IndexPass|BS=AAAAAATGGACCCATTAACC|FM=3|ND=0|FA=3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3|PV=101,102,102,101,102,114,111,114,111,113,106,113,108,113,114,114,112,113,113,111,113,87,109,113,112,113,113,113,114,111,114,113,114,111,113,113,114,113,114,106,113,112,114,113,103,113,111,113,114,114,114,112,110,111,113,113,114,114,110,112,113,113,113,114,114,112,114,113,108,108\n', 'AAATCGGGTCACTCCCACCTGAATACTGCGCTTTTCCGATCGGCTTAAAAAATGGCGCACCACGAGATTA\n', '+\n', '~~~~~~~~~~~~~~~~~~~~~x~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n']
+        singleFastqConsolidate("../data/TestR1.fastq")
+        conFq = open("TestR1.cons.fastq")
+        assert conFq.readlines() == ['@AAAAAATGGACCCATTAACC 1:N:0:ACAGTG|FP=IndexPass|BS=AAAAAATGGACCCATTAACC|FM=3|ND=0|FA=3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3|PV=101,102,102,101,102,114,111,114,111,113,106,113,108,113,114,114,112,113,113,111,113,87,109,113,112,113,113,113,114,111,114,113,114,111,113,113,114,113,114,106,113,112,114,113,103,113,111,113,114,114,114,112,110,111,113,113,114,114,110,112,113,113,113,114,114,112,114,113,108,108\n', 'AAATCGGGTCACTCCCACCTGAATACTGCGCTTTTCCGATCGGCTTAAAAAATGGCGCACCACGAGATTA\n', '+\n', '~~~~~~~~~~~~~~~~~~~~~x~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n']
+        subprocess.check_call("rm TestR1.cons.fastq",shell=True)
+        conFq.close()
 
 if __name__ == '__main__':
     unittest.main()
