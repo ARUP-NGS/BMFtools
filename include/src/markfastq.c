4ec8b16ac3bc2c7e355a9a4545b10b7e25fbf64f
dnephi <daniel.baker@aruplab.com>
2015-08-10 16:48:51 -0600

    Regex marking now works.

--- include/src/markfastq.c
+++ include/src/markfastq.c
@@ -1,12 +1,13 @@
 #include <zlib.h>
 #include <stdio.h>
-#include <regex.h>
 #include <stdint.h>
+#include <regex.h>
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
 #define KSEQ_TO_STRING(handle, read, index, pass) fprintf(handle, "%s ~#!#~|FP=%i|BS=%s\n%s\n+\n%s\n", read->name.s, pass, index->seq.s, read->seq.s, read->qual.s)
 
+#define test_hp(read, regex)
 
 int main(int argc, char *argv[])
 {
@@ -14,21 +15,28 @@ int main(int argc, char *argv[])
     gzFile fp_read, fp_index;
     kseq_t *seq;
     kseq_t *seq_index;
+    char * pch;
     int l;
     int l_index;
-    regex_t hp_regex;
-    int regex_int_ret;
     int pass;
     char buffer [20];
-    sprintf(buffer, "([ACTG])\1{%i}", 10);
-    regex_int_ret = regcomp(&hp_regex, buffer, 0);
-    if(regex_int_ret){
-        fprintf(stderr, "Could not compile regex. Abort!\n");
+    const int hp_threshold = 10;
+    sprintf(buffer, "([ACTG])\1{%i,}", hp_threshold);
+    printf("Please run.\n");
+
+    // Build regex
+    regex_t regex;
+    int reti;
+
+    reti = regcomp(&regex, buffer, 0);
+    if(reti) {
+        fprintf(stderr, "Could not compile regular expression '%s'.\n", buffer);
         exit(1);
     }
+
     if (argc == 1 || argc < 3) {
         fprintf(stderr, "Usage: %s <Fq.seq> <Index.seq> <out.seq>"
-                        "\nLeave out out.seq to emit to stdout", argv[0]);
+                        "\nLeave out out.seq to emit to stdout.\n", argv[0]);
         return 1;
     }
     if (argc == 3) {
@@ -39,7 +47,7 @@ int main(int argc, char *argv[])
         outHandle = fopen(argv[3], "w");
     }
     fp_read = gzopen(argv[1], "r");
-    fp_index = gzopen(argv[1], "r");
+    fp_index = gzopen(argv[2], "r");
     seq = kseq_init(fp_read);
     seq_index = kseq_init(fp_index);
     while ((l = kseq_read(seq)) >= 0) {
@@ -49,10 +57,13 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Index return value for kseq_read less than 0. Are the fastqs different sizes? Abort!\n");
             return 1;
         }
-        // Test regex.
-        regex_int_ret = regexec(&hp_regex, seq_index->seq.s, 0, NULL, 0);
         // Set pass or fail for record.
-        pass = regex_int_ret > 0 ? 0 : 1;
+        reti = regexec(&regex, seq_index->qual.s, 0, NULL, 0);
+        pass = (reti) ? 1 : 0;
+        if(!pass){
+            printf("Is this right? Should string %s have been failed?\n", seq_index->qual);
+            exit(1);
+        }
         KSEQ_TO_STRING(outHandle, seq, seq_index, pass);
     }
     fprintf("return value: %d\n", l);
