2f55a8660c64878b74c22294b663e9bd5e6b6a59
dnephi <daniel.baker@aruplab.com>
2015-08-27 15:49:41 -0600

    Perfect. Compiled.

--- src/dmp/dmp.c
+++ src/dmp/dmp.c
@@ -1,14 +1,14 @@
 #include "dmp.h"
 
 //Function definitions
-double_t igamc_pvalues(int num_pvalues, double_t x);
-double_t INV_CHI2_FROM_LOG10(int32_t log10int);
+longdouble_t igamc_pvalues(int num_pvalues, double_t x);
+longdouble_t INV_CHI2_FROM_LOG10(int32_t log10int);
 
 int main(int argc, char* argv[]) {
     int n;
     double f;
     n = atoi(argv[1]);
     f = atof(argv[2]);
-    fprintf(stderr, "IGAMC_PVALUES: %f", igamc_pvalues((double_t)n, (double_t)f));
+    fprintf(stderr, "IGAMC_PVALUES: %f", igamc_pvalues((longdouble_t)n, (longdouble_t)f));
     return 0;
 }
--- src/dmp/dmp.h
+++ src/dmp/dmp.h
@@ -2,28 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include "igamc_cephes.c"
 
-#include "cephes/igam.c"
 
 #define LOG10E_X5_INV = 0.46051701859880917
 //Multiply a phred score by this to convert a -10log_10(x) to a -2log_e(x)
 //such as in the following macro:
 #define LOG10_TO_CHI2(x) (x) * LOG10_X5_INV
 
-typedef double double_t;
+typedef long double longdouble_t;
 
 // Converts a 
-inline double_t igamc_pvalues(int num_pvalues, double_t x)
+inline longdouble_t igamc_pvalues(int num_pvalues, longdouble_t x)
 {
     if(x < 0) {
         return 1.0;
     }
     else {
-        return igamc(num_pvalues * 1., x / 2.0);
+        return igamcl(num_pvalues * 1., x / 2.0);
     }
 }
 
-inline double_t INV_CHI2_FROM_LOG10(int32_t log10int)
+inline longdouble_t INV_CHI2_FROM_LOG10(int32_t log10int)
 {
     return -2 * log(1 - pow(10, log10int));
 }
