0d481a89a2cc3b411bb5318ff066c19798ea980d
dnephi <daniel.baker@aruplab.com>
2015-09-04 09:47:25 -0600

    Finished destructors and cleanup for outpost hash dmp.

--- src/dmp/hash_dmp.c
+++ src/dmp/hash_dmp.c
@@ -16,6 +16,8 @@ void dmp_process_write(KingFisher_t *kfp, FILE *handle, int blen);
 void fill_csv_buffer(int readlen, int *arr, char *buffer, char *prefix);
 void fill_fa_buffer(KingFisher_t *kfp, int *agrees, char *buffer);
 void fill_pv_buffer(KingFisher_t *kfp, int *agrees, char *buffer);
+void pushback_kseq(KingFisher_t *kfp, kseq_t *seq, int *nuc_indices, int blen);
+
 KingFisher_t *init_kfp(int readlen);
 const int fisher = 137; // I just picked a number
 KHASH_MAP_INIT_INT64(fisher, uint64_t) // Set up khash with 64-bit integer keys and uint64_t payload
@@ -36,9 +38,17 @@ typedef struct Outpost {
     uint64_t bin; // bin to place the index in
 } Outpost_t;
 
-Outpost_t *INIT_OUTPOST(FILE *in_handle, FILE *out_handle, int readlen, int blen) {
+Outpost_t *INIT_OUTPOST(FILE *in_handle, FILE *out_handle) {
     gzFile fp = gzdopen(fileno(in_handle), "r");
     kseq_t *seq = kseq_init(fp);
+    int l = kseq_read(seq);
+    if(l < 0) {
+        fprintf(stderr, "Couldn't open input file handle for reading. Abort!\n");
+        exit(1);
+    }
+    int readlen = strlen(seq->seq.s);
+    char *bs_ptr = barcode_mem_view(seq);
+    int blen = infer_barcode_length(bs_ptr);
     khash_t(fisher) *hash = kh_init(fisher);
     KingFisher_t **Expedition = (KingFisher_t **)malloc(EXPEDITION_INC_SIZE * sizeof(KingFisher_t *));
     for(int i = 0; i < EXPEDITION_INC_SIZE; ++i) {
@@ -63,10 +73,12 @@ Outpost_t *INIT_OUTPOST(FILE *in_handle, FILE *out_handle, int readlen, int blen
     return ret_ptr;
 }
 
+
 void RESIZE_OUTPOST(Outpost_t *outpost) {
+    uint64_t tmpmax = outpost->max;
     outpost->max += EXPEDITION_INC_SIZE;
 
-    outpost->Expedition = (KingFisher_t **)realloc(outpost->Expedition, outpost->max * sizeof(KingFisher_t *));
+    outpost->Expedition = (KingFisher_t **)realloc(outpost->Expedition, (outpost->max + EXPEDITION_INC_SIZE) * sizeof(KingFisher_t *));
     if(!outpost->Expedition) { // Realloc failed!
         fprintf(stderr, "Reallocating memory for Outpost to final size %i failed. Abort!\n", outpost->max);
         exit(1);
@@ -76,13 +88,13 @@ void RESIZE_OUTPOST(Outpost_t *outpost) {
     }
     return;
 }
+
+
 /*
  * @returns: 1 upon success, 0 upon end of file.
  */
-int PUSHBACK_OUTPOST(Outpost_t *outpost) {
+void PUSHBACK_OUTPOST(Outpost_t *outpost) {
     outpost->bs_ptr = barcode_mem_view(outpost->seq);
-    outpost->l = kseq_read(outpost->seq);
-    if(outpost->l < 0) return 0;
     outpost->bin = get_binnerl(outpost->bs_ptr, outpost->blen);
     outpost->k = kh_get(fisher, outpost->hash, outpost->bin);
     if(outpost->k == kh_end(outpost->hash)) {
@@ -100,23 +112,49 @@ int PUSHBACK_OUTPOST(Outpost_t *outpost) {
                       outpost->seq,
                       outpost->nuc_indices, outpost->blen);
     }
-    return 1;
+    return;
 }
 
 /*
-int outpost_dmp_core(Outpost_t *outpost, )
-*/
+ * Cleans up temporary structures. Does not close either file handle.
+ * Expedition's entries also need to have been cleaned up before this is called!
+ */
+
+void destroy_outpost(Outpost_t *outpost) {
+    free(outpost->bs_ptr);
+    free(outpost->nuc_indices);
+    kseq_destroy(outpost->seq);
+    kh_destroy(fisher, outpost->hash);
+    free(outpost->Expedition);
+    return;
+}
+
+
+void write_expedition_results(Outpost_t *outpost) {
+    for(int i = 0; i < outpost->length; i++) {
+        dmp_process_write(outpost->Expedition[i], outpost->out_handle, outpost->blen);
+        destroy_kf(outpost->Expedition[i]);
+    }
+    for(int i = outpost->length; i < outpost->max; ++i) {
+        destroy_kf(outpost->Expedition[i]); // Get rid of the pre-allocated but unused KingFisher_t objects.
+    }
+}
+
+void outpost_dmp_core(FILE *in_handle, FILE *out_handle) {
+    Outpost_t *outpost = INIT_OUTPOST(in_handle, out_handle);
+    PUSHBACK_OUTPOST(outpost); // Run this just once before the loop, as I've iterated through it once to get read length and barcod length.
+    while((outpost->l = kseq_read(outpost->seq)) >= 0) {
+        PUSHBACK_OUTPOST(outpost);
+    }
+    write_expedition_results(outpost);
+    destroy_outpost(outpost);
+}
+
+
 
 //TODO: - to fix this attempted khash use.
-// 2. Write a pushback.
-// 3. Write a macro each for doing the full put/get in one command.
-// 4. Use the old pushback code.
-// 5. ???
-// 6. PROFIT
-
-void hash_dmp_core(khash_t(fisher) *hash, FILE *handle, kseq_t *seq, khiter_t k);
-void pushback_hash(khash_t(fisher) *hash, kseq_t *seq, char *bs_ptr, int blen, int readlen, int *nuc_indices, khiter_t k);
-void pushback_kseq(KingFisher_t *kfp, kseq_t *seq, int *nuc_indices, int blen);
+// 7. ???
+// 8. PROFIT
 
 void print_usage(char *argv[]) {
     fprintf(stderr, "Usage: %s -o <output_filename> <input_filename>.\n"
@@ -157,102 +195,9 @@ int main(int argc, char *argv[]){
     else {
         out_handle = fopen(outfname, "w");
     }
-    gzFile fp = gzdopen(fileno(in_handle), "r");
-    fprintf(stderr, "Now reading from file or handle %s\n", infname);
-    kseq_t *seq = kseq_init(fp);
-    khiter_t k;
-    khash_t(fisher) *hash = kh_init(fisher);
-    KingFisher_t **Expedition = (KingFisher_t **)malloc(EXPEDITION_INC_SIZE * sizeof(KingFisher_t *));
-    // Initialized kseq
-    int l = kseq_read(seq);
-    if(l < 0) {
-        fprintf(stderr, "Could not open fastq file. Abort mission!\n");
-        return 1;
-    }
-    hash_dmp_core(hash, out_handle, seq, k);
-    fprintf(stderr, "Returned from hash_dmp_core.\n");
-    kh_destroy(fisher, hash);
-    kseq_destroy(seq);
+    outpost_dmp_core(in_handle, out_handle);
+    fprintf(stderr, "Returned from outpost_dmp_core.\n");
     fclose(out_handle);
+    fclose(in_handle);
     return 0;
-}
-
-
-void hash_dmp_core(khash_t(fisher) *hash, FILE *handle, kseq_t *seq, khiter_t k) {
-    fprintf(stderr, "Now beginning hash_dmp_core.\n");
-    int *nuc_indices = malloc(2 * sizeof(int));
-    char *bs_ptr = barcode_mem_view(seq);
-    if(!bs_ptr) {
-        fprintf(stderr, "Locating barcode failed. seq's comment: %s.\n", seq->comment.s);
-        exit(1);
-    }
-    int blen = infer_barcode_length(bs_ptr);
-    fprintf(stderr, "Now getting barcode length.\n");
-    int readlen = strlen(seq->seq.s);
-    fprintf(stderr, "read length (inferred): %i\n", readlen);
-    KingFisher_t Holloway = init_kf(readlen);
-    //fprintf(stderr, "Holloway's current length: %i. Barcode: %s. Pointer to Holloway: %p.\n", Holloway.length, Holloway.barcode, &Holloway);
-    pushback_kseq(&Holloway, seq, nuc_indices, blen);
-    int ret;
-    uint64_t bin = get_binnerl(bs_ptr, blen);
-    k = kh_put(fisher, hash, get_binnerl(bs_ptr, blen), &ret);
-    /*
-    kh_value(hash, k) = &Holloway;
-    char *first_barcode = (char *)malloc((blen + 1) * sizeof(char));
-    memcpy(first_barcode, bs_ptr, blen);
-    first_barcode[blen] = '\0';
-    //pushback_hash(hash, seq, bs_ptr, blen, readlen, nuc_indices, k);
-    // Delete every between here and "int l" when done.
-    fprintf(stderr, "Holloway's current length: %i. Barcode: %s. Pointer to Holloway: %p.\n", Holloway.length, Holloway.barcode, &Holloway);
-    int l;
-    while ((l = kseq_read(seq)) >= 0) {
-        bs_ptr = barcode_mem_view(seq);
-        pushback_hash(hash, seq, bs_ptr, blen, readlen, nuc_indices, k);
-    }
-    k = kh_get(fisher, hash, bin);
-    KingFisher_t *omgz = kh_value(hash, k);
-    fprintf(stderr, "First barcode's info %i. Barcode: %s. Pointer to Holloway: %p.\n", omgz->length, omgz->barcode, omgz);
-    */
-    int barcode_counter = 0;
-    khint_t _i;
-    /*
-    for(_i = kh_begin(hash); _i != kh_end(hash); ++_i) {
-        if(!kh_exist(hash, _i)) continue;
-        fprintf(stderr, "Just got the Hook for the KingFisher_t object.\n");
-        dmp_process_write(kh_val(hash, _i), handle, blen);
-        fprintf(stderr, "Now let's just destroy Hook.\n");
-        //destroy_kf(Hook);
-        barcode_counter++;
-    }
-    */
-    fprintf(stderr, "Number of unique barcode: %i\n", barcode_counter);
-    //dmp_process_write(Hook, handle, bs_ptr, blen)
-    free(nuc_indices);
-}
-
-//int parallel_hash_dmp(char )
-
-void pushback_hash(khash_t(fisher) *hash, kseq_t *seq, char *bs_ptr, int blen, int readlen, int *nuc_indices, khiter_t k) {
-    //KingFisher_t *Hook = kh_get_val(fisher, hash, get_binner(bs_ptr, blen), NULL, k);
-    /*
-    fprintf(stderr, "Read length: %i.\n", readlen);
-    int ret;
-    k = kh_get(fisher, hash, get_binner(bs_ptr, blen));
-    if(k == kh_end(hash)) {
-        KingFisher_t Holloway = init_kf(readlen);
-        k = kh_put(fisher, hash, get_binner(bs_ptr, blen), &ret);
-        pushback_kseq(&Holloway, seq, nuc_indices, blen);
-        kh_value(hash, k) = &Holloway;
-        fprintf(stderr, "Read length for the hash table after being put in: %i\n", Holloway.readlen);
-        fprintf(stderr, "Length, barcode after being put in: %i, %s\n", Holloway.length, Holloway.barcode);
-        fprintf(stderr, "Value of pointer: %p\n", &Holloway);
-    }
-    else {
-        fprintf(stderr, "Read length of accessed value: %i\n", kh_value(hash, k)->readlen);
-        fprintf(stderr, "Length of accessed value: %i\n", kh_value(hash, k)->length);
-        fprintf(stderr, "Value of pointer: %p\n", kh_value(hash, k));
-        pushback_kseq(kh_value(hash, k), seq, nuc_indices, blen);
-    }
-    */
-    return;
 }
