1d2468abc2b62c8cb023f6b2a8fb456544ff077e
dnephi <daniel.baker@aruplab.com>
2015-09-17 12:10:36 -0600

    * Update PV (Done.) * Update FA (Done.) * Update FM (Done.) * Add NC (Number Changed) tag. (Done.) * Add NN (Number N'd) tag. (Done.) * Update RC tag. (Done.) * Update the quality score string. (Done.) * Fill in the bam_rescue settings struct (Done.) * Add in commmandline options. (Done.) * Write the settings destroyer. (Done.)

--- src/holloway/bam_rescue.c
+++ src/holloway/bam_rescue.c
@@ -37,23 +37,20 @@ DEALINGS IN THE SOFTWARE.  */
 
 typedef bam1_t *bam1_p;
 
-#include "htslib/khash.h"
-KHASH_SET_INIT_STR(name)
-KHASH_MAP_INIT_INT64(pos, bam1_p)
-KHASH_MAP_INIT_STR(rescue, bam1_p)
-
-#define BUFFER_SIZE 0x40000
-
-typedef struct {
-    uint64_t n_checked, n_removed;
-    khash_t(pos) *best_hash;
-} lib_aux_t;
-KHASH_MAP_INIT_STR(lib, lib_aux_t)
 
 typedef struct tmp_stack {
     int n, max;
     bam1_t **a;
 } tmp_stack_t;
+
+typedef struct rescue_settings {
+    FILE *fp;
+    int write_nc2bam; // Write the reads with the number of nucleotides changed to bam rather than to a fastq file.
+    int hd_thresh; // Threshold for hamming distance. If < hd_thresh, reads are considered to be from the same family.
+    int is_se; // Is single end
+    int force_se; // Is single end
+    char *fq_fname;
+} rescue_settings_t;
 
 
 // arr1 and arr2 should each be a uint64_t buf[2] or a malloc'd char * of size 2.
@@ -84,13 +81,52 @@ static inline void stack_insert(tmp_stack_t *stack, bam1_t *b)
     stack->a[stack->n++] = b;
 }
 
-static inline void write_stack(tmp_stack_t *stack, samFile *out, bam_hdr_t *hdr)
+    /*
+     * TODO:
+     * Update PV (Done.)
+     * Update FA (Done.)
+     * Update FM (Done.)
+     * Add NC (Number Changed) tag. (Done.)
+     * Add NN (Number N'd) tag. (Done.)
+     * Update RC tag. (Done.)
+     * Update the quality score string. (Done.)
+     * Fill in the bam_rescue settings struct (Done.)
+     * Add in commmandline options. (Done.)
+     * Write the settings destroyer. (Done.)
+     *
+     * Yet TODO
+     * Fill in write_bam1_nc (for reads to be realigned)
+     * Add in commandline option key for users.
+     * Add in the count for RC to crms
+     * Expand the max_phreds in all of the marksplits to make sure it's the highest quality score for the base that was called as consensus.
+     * Expand the rescaling to 4 dimensions
+     */
+
+static inline void write_bam1_nc(bam1_t *b, FILE *fp)
+{
+    fprintf(stderr, "Not implemented (write_bam1_nc). Abort mission!\n");
+    exit(EXIT_FAILURE);
+}
+
+
+static inline void write_stack(tmp_stack_t *stack, samFile *out, bam_hdr_t *hdr, rescue_settings_t *settings)
 {
     int i;
     for (i = 0; i != stack->n; ++i) {
         if(stack->a[i]) { // Since I'll be clearing out the values after updating, I am checking to see if it's not NULL first.
-            sam_write1(out, hdr, stack->a[i]);
+            if(bam_aux2i(stack->a[i], "NC") || bam_aux2i(stack->a[i], "NN")) { // If NN or NC are non-zero.
+                if(settings->write_ncbam) {
+                    sam_write1(out, hdr, stack->a[i]);
+                }
+                else {
+                    write_bam1_nc(stack->a[i], fp);
+                }
+            }
+            else {
+                sam_write1(out, hdr, stack->a[i]);
+            }
             bam_destroy1(stack->a[i]);
+            stack->a[i] = NULL;
         }
     }
     stack->n = 0;
@@ -110,54 +146,110 @@ static inline int hamming_dist_test(char *bs1, char *bs2, int len, int hd_thresh
     return 0;
 }
 
-static inline void update_bam1(bam1_t *p, bam1_t *b)
+static inline int *bam_aux2ip(uint8_t *ptr)
 {
-    int realign = 0;
+    return (int *)(++ptr);
+}
+
+static inline int disc_pvalues(double pv_better, double pv_worse)
+{
+    return pvalue_to_phred(igamc(2., LOG10_TO_CHI2(pv_better - (10. * log10(1 - pow(10., (pv_worse * 0.1)))))));
+}
+
+static inline void update_int_tag(bam1_t *b, const char[2]key, int increment)
+{
+    uint8_t *tag_ptr = bam_aux_get(b, key);
+    if(!tag_ptr) {
+        bam_aux_append(b, key, 'i', sizeof(int), (uint8_t *)&increment)
+    }
+    else {
+        (int)*(++tag_ptr) += increment;
+    }
+    return;
+}
+
+static inline void update_int_ptr(uint8_t *ptr1, uint8_t *inc_ptr)
+{
+    if(!inc_ptr) {
+        fprintf(stderr, "Missing RC tag. Abort mission!\n");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        (int)*(++ptr1) += (int)*(++inc_ptr);
+    }
+    return;
+}
+
+static inline void update_bam1(bam1_t *p, bam1_t *b, FILE *fp)
+{
+    int n_changed = 0;
+    int mask = 0;
     uint8_t *pPVptr = bam_aux_get(p, "PV");
     uint8_t *bPVptr = bam_aux_get(b, "PV");
+    uint8_t *pFAptr = bam_aux_get(p, "FA");
+    uint8_t *bFAptr = bam_aux_get(b, "FA");
 #if !NDEBUG
     if(!bPVptr || !pPVptr) {
         fprintf(stderr, "Required PV tag not found. Abort mission!\n");
         exit(EXIT_FAILURE);
     }
+    if(!bFAptr || !pFAptr) {
+        fprintf(stderr, "Required FA tag not found. Abort mission!\n");
+        exit(EXIT_FAILURE);
+    }
 #endif
     int *bPV = (int *)bPVptr; // Length of this should be b->l_qseq
     int *pPV = (int *)pPVptr; // Length of this should be p->l_qseq
+    int *bFA = (int *)bFAptr; // Length of this should be b->l_qseq
+    int *pFA = (int *)pFAptr; // Length of this should be p->l_qseq
+    int *pFM = bam_aux2ip(bam_aux_get(p, "FM"));
+    *pFM += bam_aux2i(bam_aux_get(b, "FM"));
+
     char *bSeq = (char *)bam_get_seq(b);
     char *pSeq = (char *)bam_get_seq(p);
+    char *bQual = (char *)bam_get_qual(b);
+    char *pQual = (char *)bam_get_qual(p);
     for(int i = 0; i < p->core.l_qseq; ++i) {
         if(bSeq[i] == pSeq[i]) {
-            pPV[i] = igamc((double)2, LOG10_TO_CHI2(pPV[i] + bPV[i]) / 2.0);
+            pPV[i] = igamc(2., LOG10_TO_CHI2(pPV[i] + bPV[i]) / 2.0);
+            pFA[i] += bFA[i];
+            if(bQual[i] > pQual[i])
+                pQual[i] = bQual[i];
         }
         else {
             if(pPV[i] > bPV[i]) {
-                pPV[i] -= bPV[i];
+                pPV[i] = disc_pvalues(pPV[i], bPV[i]);
+                // Leave pFA alone since it didn't change.
             }
             else {
+                pPV[i] = disc_pvalues(bPV[i], pPV[i]);
                 pSeq[i] = bSeq[i];
-                pPV[i] = bPV[i] - pPV[i];
-                realign = 1;
+                pFA[i] = bFA[i];
+                pQual[i] = bQual[i];
+                ++n_changed;
             }
             if(pPV[i] < 3) {
                 pSeq[i] = 'N';
-                realign = 1;
+                pFA[i] = 0;
+                pPV[i] = 0;
+                pQual[i] = '!';
+                ++mask;
             }
         }
 
-        // TODO: Check for a merged tag.
         // Add if not present.
-        if(realign) {
-            // TODO: Check for a realign tag.
-            fprintf(stderr, "Not implemented yet!\n");
-            exit(EXIT_FAILURE);
-            //bam_aux_append(b, "RA", )
+        update_int_tag(p, "NC", n_changed);
+        update_int_tag(p, "NN", mask);
+        uint8_t *p_rc_ptr = bam_aux_get(p, "RC");
+        if(p_rc_ptr) {
+            update_int_ptr(p_rc_ptr, bam_aux_get(b, "RC"));
         }
         bam_destroy1(b);
         b = NULL;
     }
 }
 
-static inline void flatten_stack(tmp_stack_t *stack, int len, int hd_thresh)
+static inline void flatten_stack(tmp_stack_t *stack, rescue_settings_t *settings_ptr)
 {
     int pass;
     bam1_t *b, bam1_t *p;
@@ -192,12 +284,7 @@ static inline void flatten_stack(tmp_stack_t *stack, int len, int hd_thresh)
     return;
 }
 
-typedef struct rescue_settings {
-    int len;
-    int hd_thresh;
-} rescue_settings_t;
-
-void bam_rescue_core(samFile *in, bam_hdr_t *hdr, samFile *out, rescue_settings_t *settings)
+void bam_rescue_core(samFile *in, bam_hdr_t *hdr, samFile *out, rescue_settings_t *settings_ptr)
 {
     bam1_t *b, *tmpb;
     int last_tid = -1;
@@ -212,52 +299,30 @@ void bam_rescue_core(samFile *in, bam_hdr_t *hdr, samFile *out, rescue_settings_
         bam1_core_t *c = &b->core;
         ARR_SETKEY(b, current_arr);
         if (!ARR_CMP(last_arr, current_arr)) {
-            flatten_stack(&stack, settings->len, settings->hd_thresh);
-            write_stack(&stack, out, hdr); // write the result
-                if ((int)c->tid == -1) { // append unmapped reads
-                    sam_write1(out, hdr, b);
-                    while (sam_read1(in, hdr, b) >= 0) sam_write1(out, hdr, b);
-                    break;
-                }
-                if(last_tid != c->tid) {
-                    last_tid = c->tid;
-                    fprintf(stderr, "[bam_rescue_core] processing reference %s...\n", hdr->target_name[c->tid]);
-                }
+            flatten_stack(&stack, settings_ptr);
+            write_stack(&stack, out, hdr, settings_ptr); // write the result
+            if ((int)c->tid < 0) { // append unmapped reads
+                sam_write1(out, hdr, b);
+                while (sam_read1(in, hdr, b) >= 0) sam_write1(out, hdr, b);
+                break;
             }
+            if(last_tid != c->tid) {
+                last_tid = c->tid;
+                fprintf(stderr, "[bam_rescue_core] processing reference %s...\n", hdr->target_name[c->tid]);
+            }
+        }
         if (!(c->flag&BAM_FPAIRED)) {
             sam_write1(out, hdr, b);
         } else { // paired, head
-            int pass = 1; // For a missing BS tag.
-            /*
-             * OKAY, NOW MY CODE
-             */
-                if (ret == 0)
-                    fprintf(stderr, "[bam_rescue_core] inconsistent BAM file for pair '%s'. Continue anyway.\n", bam_get_qname(b));
-            } else { // not found in best_hash
-                tmpb = bam_dup1(b);
-                stack_insert(&stack, tmpb);
-            }
-    }
-
-    for (k = kh_begin(aux); k != kh_end(aux); ++k) {
-        if (kh_exist(aux, k)) {
-            lib_aux_t *q = &kh_val(aux, k);
-            write_stack(&stack, out, hdr);
-            fprintf(stderr, "[bam_rescue_core] %lld / %lld = %.4lf in library '%s'\n", (long long)q->n_removed,
-                    (long long)q->n_checked, (double)q->n_removed/q->n_checked, kh_key(aux, k));
-            kh_destroy(pos, q->best_hash);
-            free((char*)kh_key(aux, k));
+            tmpb = bam_dup1(b);
+            stack_insert(&stack, tmpb);
         }
     }
-    kh_destroy(lib, aux);
-
-    clear_del_set(del_set);
-    kh_destroy(name, del_set);
     free(stack.a);
     bam_destroy1(b);
 }
 
-void bam_rescue_se_core(samFile *in, bam_hdr_t *hdr, samFile *out, int force_se)
+void bam_rescue_se_core(samFile *in, bam_hdr_t *hdr, samFile *out, rescue_settings_t *settings_ptr)
 {
     fprintf(stderr, "Single-end rescue not implemented. Abort mission! (Or you could code it -- let me know!)\n");
     exit(EXIT_FAILURE);
@@ -275,7 +340,23 @@ static int rescue_usage(void) {
 
 int bam_rescue(int argc, char *argv[])
 {
-    int c, is_se = 0, force_se = 0;
+    /*
+     *
+typedef struct rescue_settings {
+    int write_nc2bam; // Write the reads with the number of nucleotides changed to bam rather than to a fastq file.
+    int hd_thresh; // Threshold for hamming distance. If < hd_thresh, reads are considered to be from the same family.
+    int is_se; // Is single end
+    int force_se; // Is single end
+} rescue_settings_t;
+     */
+    rescue_settings_t settings = {
+            .write_nc2bam = 0,
+            .hd_thresh = 2,
+            .force_se = 0,
+            .is_se = 0,
+            .fp = NULL,
+            .fq_fname = NULL
+    };
     samFile *in, *out;
     bam_hdr_t *header;
     char wmode[3] = {'w', 'b', 0};
@@ -286,14 +367,35 @@ int bam_rescue(int argc, char *argv[])
         { NULL, 0, NULL, 0 }
     };
 
-    while ((c = getopt_long(argc, argv, "sS", lopts, NULL)) >= 0) {
+    int c;
+    while ((c = getopt_long(argc, argv, "sSpt:", lopts, NULL)) >= 0) {
         switch (c) {
-        case 's': is_se = 1; break;
-        case 'S': force_se = is_se = 1; break;
+        case 's': settings.is_se = 1; break;
+        case 'S': settings.force_se = settings.is_se = 1; break;
+        case 't': settings.hd_thresh = atoi(optarg); break;
+        case 'p': settings.write_nc2bam = 1; break;
+        case 'f': if(optarg[0] == '-') {
+                      settings.fq_fname = strdup("stdout");
+                      settings.fp = stdout;
+                  }
+                  else {
+                      settings.fq_fname = trim_ext(optarg);
+                      settings.fq_fname = realloc(settings.fq_fname, strlen(settings.fq_fname) + 10);
+                      strcat(settings.fq_fname, ".ra.fastq");
+                      settings.fp = fopen(settings.fq_fname, "w");
+                  }
+                  break;
         default:  if (parse_sam_global_opt(c, optarg, lopts, &ga) == 0) break;
             /* else fall-through */
         case '?': return rescue_usage();
         }
+    }
+
+    if(settings.write_nc2bam && settings.fp) {
+        fprintf(stderr, "Conflicting options - write to bam and to fastq. "
+                        "This will result in non-unique primary alignments "
+                        "for a given read name. Abort mission!\n");""
+        exit(EXIT_FAILURE);
     }
     if (optind + 2 > argc)
         return rescue_usage();
@@ -313,9 +415,16 @@ int bam_rescue(int argc, char *argv[])
     }
     sam_hdr_write(out, header);
 
-    if (is_se) bam_rescue_se_core(in, header, out, force_se);
-    else bam_rescue_core(in, header, out);
+    if (settings.is_se) bam_rescue_se_core(in, header, out, &settings);
+    else bam_rescue_core(in, header, out, &settings);
     bam_hdr_destroy(header);
     sam_close(in); sam_close(out);
+    if(settings.fp) {
+        fclose(fp);
+    };
+    if(settings.fq_fname) {
+        free(settings.fq_fname);
+        settings.fq_fname = NULL;
+    }
     return 0;
 }
