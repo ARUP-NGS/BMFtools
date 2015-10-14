f05b314378a0d6edebb5dad4679b84e85bbba83a
dnephi <daniel.baker@aruplab.com>
2015-10-13 19:35:37 -0600

    Converted tabs to spaces for some files. Added a fast_atoi for naive us and an 'fp_atoi' for things which we can assume are positive integers.

--- src/dmp/include/cstr_utils.h
+++ src/dmp/include/cstr_utils.h
@@ -14,10 +14,38 @@
 inline char *trim_ext(char *fname)
 {
 	fprintf(stderr, "Now trimming char * %s.\n", fname);
-    char *buf = malloc((strlen(fname) + 1) * sizeof(char ));
-    ptrdiff_t pos = strrchr(fname, '.') - fname; // Find the position in the read where the last '.' is.
-    memcpy(buf, fname, pos * sizeof(char));
-    buf[pos] = '\0';
-    fprintf(stderr, "tmp buffer: %s.\n", buf);
-    return buf;
+	char *buf = malloc((strlen(fname) + 1) * sizeof(char ));
+	ptrdiff_t pos = strrchr(fname, '.') - fname; // Find the position in the read where the last '.' is.
+	memcpy(buf, fname, pos * sizeof(char));
+	buf[pos] = '\0';
+	fprintf(stderr, "tmp buffer: %s.\n", buf);
+	return buf;
+}
+/*
+ * Fast positive atoi
+ */
+inline int fp_atoi(char *str)
+{
+	int ret = *str++ - '0';
+	while(*str) {
+		ret = ret*10 + (*str++ - '0');
+	}
+	return ret;
+}
+
+
+inline int fast_atoi(char *str)
+{
+	int ret = 0;
+	int sign = 1;
+	switch(*str) {
+	    case '-': sign = -1; break;
+	    case '+': break;
+	    default: ret = *str - '0';
+	}
+	++str;
+	while(*str) {
+		ret = ret*10 + (*str++ - '0');
+	}
+	return ret * sign;
 }
