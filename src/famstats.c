a3f01ef60cfdf446200acc166fbfdf56ab4e729d
dnephi <daniel.baker@aruplab.com>
2015-10-30 00:08:04 -0600

    Expanded famstats to have subcommands.

--- src/famstats.c
+++ src/famstats.c
@@ -16,6 +16,10 @@ KHASH_MAP_INIT_INT64(rc, uint64_t)
 
 int RVWarn = 1;
 
+int frac_usage_exit(FILE *fp, int code) {
+	return 0;
+}
+
 
 typedef struct famstats {
 	uint64_t n_pass;
@@ -46,7 +50,7 @@ static inline void print_hashstats(famstats_t *stats)
 			continue;
 		fprintf(stdout, "%"PRIu64"\t%"PRIu64"\n", kh_key(stats->fm, stats->ki), kh_val(stats->fm, stats->ki));
 	}
-	fprintf(stdout, "#RV'd in family\tNumber of families\n");
+	fprintf(stderr, "#RV'd in family\tNumber of families\n");
 	for(stats->ki = kh_begin(stats->rc); stats->ki != kh_end(stats->rc); ++stats->ki) {
 		if(!kh_exist(stats->rc, stats->ki))
 			continue;
@@ -89,7 +93,7 @@ static inline void famstat_loop(famstats_t *s, bam1_t *b, famstat_settings_t *se
 #if DBG
 	fprintf(stderr, "FM tag: %i.\n", FM);
 #endif
-	if(b->core.qual < settings->minMQ || FM < settings->minFM || b->core.flag & 2944) {
+	if(b->core.flag & 2944 || b->core.qual < settings->minMQ || FM < settings->minFM) {
 		++s->n_fail;
 		return;
 		// Skips supp/second/read2/qc fail/marked duplicate
@@ -98,7 +102,7 @@ static inline void famstat_loop(famstats_t *s, bam1_t *b, famstat_settings_t *se
 	data = bam_aux_get(b, "RV");
 	if(!data && RVWarn) {
 		RVWarn = 0;
-		fprintf(stderr, "Warning: RV tag not found. Continue.\n");
+		fprintf(stderr, "[famstat_core]: Warning: RV tag not found. Continue.\n");
 	}
 	int RV = data ? bam_aux2i(data): 0;
 #if DBG
@@ -141,7 +145,7 @@ famstats_t *famstat_core(samFile *fp, bam_hdr_t *h, famstat_settings_t *settings
 	while ((ret = sam_read1(fp, h, b)) >= 0) {
 		famstat_loop(s, b, settings);
 		if(!(++count % 250000))
-			fprintf(stderr, "Number of records processed: %"PRIu64".\n", count);
+			fprintf(stderr, "[famstat_core] Number of records processed: %"PRIu64".\n", count);
 	}
 	bam_destroy1(b);
 	if (ret != -1)
@@ -152,23 +156,31 @@ famstats_t *famstat_core(samFile *fp, bam_hdr_t *h, famstat_settings_t *settings
 static void usage_exit(FILE *fp, int exit_status)
 {
 	fprintf(fp, "Usage: famstat <in.bam>\n");
-	fprintf(fp, "Opts:\n-m Set minimum mapping quality. Default: 0.\n");
+	fprintf(fp, "Subcommands: \nfm\tFamily Size stats\n");
+	exit(exit_status);
+}
+
+static void fm_usage_exit(FILE *fp, int exit_status)
+{
+	fprintf(fp, "Usage: famstat fm <opts> <in.bam>\n");
+	fprintf(fp, "-m Set minimum mapping quality. Default: 0.\n");
 	fprintf(fp, "-f Set minimum family size. Default: 0.\n");
 	exit(exit_status);
 }
 
-int main(int argc, char *argv[])
+int fm_main(int argc, char *argv[])
 {
 	samFile *fp;
 	bam_hdr_t *header;
 	famstats_t *s;
 	int c;
 
+
 	famstat_settings_t *settings = (famstat_settings_t *)calloc(1, sizeof(famstat_settings_t));
 	settings->minMQ = 0;
 	settings->minFM = 0;
 
-	while ((c = getopt(argc, argv, "m:f:")) >= 0) {
+	while ((c = getopt(argc, argv, "m:f:h")) >= 0) {
 		switch (c) {
 		case 'm':
 			settings->minMQ = atoi(optarg); break;
@@ -182,7 +194,7 @@ int main(int argc, char *argv[])
 			usage_exit(stderr, EXIT_FAILURE);
 		}
 	}
-	fprintf(stderr, "[fam_stat]: Running main with minMQ %i and minFM %i.\n", settings->minMQ, settings->minFM);
+	fprintf(stderr, "[famstat_main]: Running main with minMQ %i and minFM %i.\n", settings->minMQ, settings->minFM);
 
 	if (argc != optind+1) {
 		if (argc == optind) usage_exit(stdout, EXIT_SUCCESS);
@@ -190,31 +202,16 @@ int main(int argc, char *argv[])
 	}
 	fp = sam_open(argv[optind], "r");
 	if (fp == NULL) {
-		fprintf(stderr, "Cannot open input file \"%s\"", argv[optind]);
+		fprintf(stderr, "[famstat_main]: Cannot open input file \"%s\"", argv[optind]);
 		exit(EXIT_FAILURE);
 	}
 
 	header = sam_hdr_read(fp);
 	if (header == NULL) {
-		fprintf(stderr, "Failed to read header for \"%s\"\n", argv[optind]);
+		fprintf(stderr, "[famstat_main]: Failed to read header for \"%s\"\n", argv[optind]);
 		exit(EXIT_FAILURE);
 	}
 	s = famstat_core(fp, header, settings);
-	/*
-	printf("%lld + %lld in total (QC-passed reads + QC-failed reads)\n", s->n_reads[0], s->n_reads[1]);
-	printf("%lld + %lld secondary\n", s->n_secondary[0], s->n_secondary[1]);
-	printf("%lld + %lld supplementary\n", s->n_supp[0], s->n_supp[1]);
-	printf("%lld + %lld duplicates\n", s->n_dup[0], s->n_dup[1]);
-	printf("%lld + %lld mapped (%s : %s)\n", s->n_mapped[0], s->n_mapped[1], percent(b0, s->n_mapped[0], s->n_reads[0]), percent(b1, s->n_mapped[1], s->n_reads[1]));
-	printf("%lld + %lld paired in sequencing\n", s->n_pair_all[0], s->n_pair_all[1]);
-	printf("%lld + %lld read1\n", s->n_read1[0], s->n_read1[1]);
-	printf("%lld + %lld read2\n", s->n_read2[0], s->n_read2[1]);
-	printf("%lld + %lld properly paired (%s : %s)\n", s->n_pair_good[0], s->n_pair_good[1], percent(b0, s->n_pair_good[0], s->n_pair_all[0]), percent(b1, s->n_pair_good[1], s->n_pair_all[1]));
-	printf("%lld + %lld with itself and mate mapped\n", s->n_pair_map[0], s->n_pair_map[1]);
-	printf("%lld + %lld singletons (%s : %s)\n", s->n_sgltn[0], s->n_sgltn[1], percent(b0, s->n_sgltn[0], s->n_pair_all[0]), percent(b1, s->n_sgltn[1], s->n_pair_all[1]));
-	printf("%lld + %lld with mate mapped to a different chr\n", s->n_diffchr[0], s->n_diffchr[1]);
-	printf("%lld + %lld with mate mapped to a different chr (mapQ>=5)\n", s->n_diffhigh[0], s->n_diffhigh[1]);
-	*/
 	print_stats(s);
 	kh_destroy(fm, s->fm);
 	kh_destroy(rc, s->rc);
@@ -223,4 +220,90 @@ int main(int argc, char *argv[])
 	bam_hdr_destroy(header);
 	sam_close(fp);
 	return 0;
+}
+
+static inline void frac_loop(bam1_t *b, int minFM, uint64_t *fm_above, uint64_t *fm_total)
+{
+	uint8_t *data = bam_aux_get(b, "FM");
+	tag_test(data, "FM");
+	int FM = bam_aux2i(data);
+	if(b->core.flag & 2944) {
+		return;
+		// Skips supp/second/read2/qc fail/marked duplicate
+		// 2944 is equivalent to BAM_FSECONDARY | BAM_FSUPPLEMENTARY | BAM_QCFAIL | BAM_FISREAD2
+	}
+	if(FM >= minFM)
+		*fm_above += FM;
+	*fm_total += FM;
+}
+
+
+int frac_main(int argc, char *argv[])
+{
+	samFile *fp;
+	bam_hdr_t *header;
+	int c;
+	uint32_t minFM;
+
+	if(strcmp(argv[1], "--help") == 0) frac_usage_exit(stderr, EXIT_SUCCESS);
+
+	while ((c = getopt(argc, argv, "m:h?")) >= 0) {
+		switch (c) {
+		case 'm':
+			minFM = (uint32_t)atoi(optarg); break;
+			break;
+		case '?':
+		case 'h':
+			frac_usage_exit(stderr, EXIT_SUCCESS);
+		default:
+			frac_usage_exit(stderr, EXIT_FAILURE);
+		}
+	}
+	fprintf(stderr, "[famstat_frac_main]: Running frac main minFM %i.\n", minFM);
+
+	if (argc != optind+1) {
+		if (argc == optind) usage_exit(stdout, EXIT_SUCCESS);
+		else usage_exit(stderr, EXIT_FAILURE);
+	}
+	fp = sam_open(argv[optind], "r");
+	if (fp == NULL) {
+		fprintf(stderr, "[famstat_frac_main]: Cannot open input file \"%s\"", argv[optind]);
+		exit(EXIT_FAILURE);
+	}
+
+	header = sam_hdr_read(fp);
+	if (header == NULL) {
+		fprintf(stderr, "[famstat_main]: Failed to read header for \"%s\"\n", argv[optind]);
+		exit(EXIT_FAILURE);
+	}
+	uint64_t fm_above = 0, total_fm = 0, count = 0;
+	bam1_t *b = bam_init1();
+	while ((c = sam_read1(fp, header, b)) >= 0) {
+		frac_loop(b, minFM, &fm_above, &total_fm);
+		if(!(++count % 250000))
+			fprintf(stderr, "[famstat_frac_core] Number of records processed: %"PRIu64".\n", count);
+	}
+	fprintf(stderr, "#Fraction of raw reads with >= minFM %i: %f.\n", minFM, (double)fm_above / total_fm);
+	bam_destroy1(b);
+	bam_hdr_destroy(header);
+	sam_close(fp);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2) {
+		usage_exit(stderr, 1);
+	}
+	if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+		usage_exit(stderr, 0);
+	}
+	if(strcmp(argv[1], "fm") == 0) {
+		return fm_main(argc - 1, argv + 1);
+	}
+	if(strcmp(argv[1], "frac") == 0) {
+		return frac_main(argc - 1, argv + 1);
+	}
+	fprintf(stderr, "Unrecognized subcommand. See usage.\n");
+	usage_exit(stderr, 1);
 }
