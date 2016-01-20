db233ac49cac66256820952c082db8080679ceca
dnephi <daniel.baker@aruplab.com>
2016-01-20 10:25:46 -0700

    Fixed famstats.

--- src/bmf_famstats.c
+++ src/bmf_famstats.c
@@ -2,7 +2,7 @@
 
 int RVWarn = 1;
 
-int frac_usage_exit(FILE *fp, int code) {
+int famstats_frac_usage_exit(FILE *fp, int code) {
 	fprintf(fp, "bmftools famstats frac <opts> <in.bam>\n"
 			"Opts:\n-m minFM to accept. REQUIRED.\n"
 			"-h, -?: Return usage.\n");
@@ -20,30 +20,52 @@ int fm_cmp(const void *fm1, const void *fm2)
 	return ((fm_t *)fm1)->fm - ((fm_t *)fm2)->fm;
 }
 
+int get_nbins(khash_t(fm) *table)
+{
+	int ret = 0;
+	for(khiter_t k = kh_begin(table); k != kh_end(table); ++k) {
+		if(!kh_exist(table, k)) continue;
+		++ret;
+	}
+	return ret;
+}
+
 
 static void print_hashstats(famstats_t *stats, FILE *fp)
 {
-	fm_t *fms = (fm_t *)malloc(sizeof(fm_t) * MAX2(stats->fm->n_occupied, stats->rc->n_occupied));
-	int i = 0;
-	fprintf(fp, "#Family size\tNumber of families\n");
-	for(stats->ki = kh_begin(stats->fm); stats->ki != kh_end(stats->fm); ++stats->ki) {
-		if(!kh_exist(stats->fm, stats->ki)) continue;
-		fms[i++] = (fm_t){.fm = kh_key(stats->fm, stats->ki), .n = kh_val(stats->fm, stats->ki)};
+	size_t size = MAX2(stats->fm->n_occupied, stats->rc->n_occupied);
+	fm_t *fms = (fm_t *)malloc(sizeof(fm_t) * size);
+	if(get_nbins(stats->fm) != stats->fm->n_occupied) {
+		LOG_ERROR("WHY DON'T THESE ADD UP? %i, %u.\n", get_nbins(stats->fm), stats->fm->n_occupied);
 	}
+	int i;
+	fprintf(fp, "#Family size\tNumber of families\n");
+	LOG_DEBUG("Copy FMs over into fm_t array. Size: %lu. fm size: %u. rc size: %u.\n", size, stats->fm->n_occupied, stats->rc->n_occupied);
+	for(i = 0, stats->ki = kh_begin(stats->fm); stats->ki != kh_end(stats->fm); ++stats->ki) {
+		if(!kh_exist(stats->fm, stats->ki)) continue;
+		LOG_DEBUG("Accessing index %i of fms.\n", i);
+		fms[i++] = (fm_t){.fm = kh_key(stats->fm, stats->ki), .n = kh_val(stats->fm, stats->ki)};
+		LOG_DEBUG("Value of i after incrementing but not using: %i.\n", i);
+	}
+	LOG_DEBUG("Qsorting fm_t array.\n");
 	qsort(fms, stats->fm->n_occupied, sizeof(fm_t), fm_cmp);
 	for(i = 0; i < stats->fm->n_occupied; ++i) fprintf(fp, "%lu\t%lu\n", fms[i].fm, fms[i].n);
 	fprintf(fp, "#RV'd in family\tNumber of families\n");
-	for(stats->ki = kh_begin(stats->rc); stats->ki != kh_end(stats->rc); ++stats->ki) {
+	LOG_DEBUG("Copy RVs over into fm_t array. Size: %lu.\n", size);
+	for(i = 0, stats->ki = kh_begin(stats->rc); stats->ki != kh_end(stats->rc); ++stats->ki) {
 		if(!kh_exist(stats->rc, stats->ki)) continue;
+		LOG_DEBUG("Accessing index %i of fms.\n", i);
 		fms[i++] = (fm_t){.fm = kh_key(stats->rc, stats->ki), .n = kh_val(stats->rc, stats->ki)};
 	}
+	LOG_DEBUG("Qsorting fm_t array.\n");
 	qsort(fms, stats->rc->n_occupied, sizeof(fm_t), fm_cmp);
+	LOG_DEBUG("Done qsorting fm_t array.\n");
 	for(i = 0; i < stats->rc->n_occupied; ++i) fprintf(fp, "%lu\t%lu\n", fms[i].fm, fms[i].n);
 	free(fms);
 }
 
 
-int target_usage_exit(FILE *fp, int success)
+int famstats_target_usage_exit(FILE *fp, int success)
 {
 	fprintf(fp, "Usage: bmftools famstats target <opts> <in.bam>\nOpts:\n-b Path to bed file.\n"
 			"-p padding. Number of bases around bed regions to pad. Default: 25.\n"
@@ -62,7 +84,7 @@ static inline void target_loop(bam1_t *b, khash_t(bed) *bed, uint64_t *fm_target
 }
 
 
-int target_main(int argc, char *argv[])
+int famstats_target_main(int argc, char *argv[])
 {
 	samFile *fp;
 	bam_hdr_t *header;
@@ -72,9 +94,9 @@ int target_main(int argc, char *argv[])
 	uint32_t padding = (uint32_t)-1;
 	uint64_t notification_interval = 1000000;
 
-	if(argc < 4) return target_usage_exit(stderr, EXIT_SUCCESS);
+	if(argc < 4) return famstats_target_usage_exit(stderr, EXIT_SUCCESS);
 
-	if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) return target_usage_exit(stderr, EXIT_SUCCESS);
+	if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) return famstats_target_usage_exit(stderr, EXIT_SUCCESS);
 
 	while ((c = getopt(argc, argv, "b:p:n:h?")) >= 0) {
 		switch (c) {
@@ -89,7 +111,7 @@ int target_main(int argc, char *argv[])
 			break;
 		case '?':
 		case 'h':
-			return target_usage_exit(stderr, EXIT_SUCCESS);
+			return famstats_target_usage_exit(stderr, EXIT_SUCCESS);
 		}
 	}
 
@@ -100,11 +122,11 @@ int target_main(int argc, char *argv[])
 	}
 
 	if (argc != optind+1)
-		return (argc == optind) ? target_usage_exit(stdout, EXIT_SUCCESS): target_usage_exit(stderr, EXIT_FAILURE);
+		return (argc == optind) ? famstats_target_usage_exit(stdout, EXIT_SUCCESS): famstats_target_usage_exit(stderr, EXIT_FAILURE);
 
 	if(!bedpath) {
 		fprintf(stderr, "[E:%s] Bed path required for famstats target. See usage.\n", __func__);
-		return target_usage_exit(stderr, EXIT_FAILURE);
+		return famstats_target_usage_exit(stderr, EXIT_FAILURE);
 	}
 
 	fp = sam_open(argv[optind], "r");
@@ -121,13 +143,15 @@ int target_main(int argc, char *argv[])
 	bam1_t *b = bam_init1();
 	while (LIKELY((c = sam_read1(fp, header, b)) >= 0)) {
 		target_loop(b, bed, &fm_target, &total_fm);
-		if(UNLIKELY(++count % notification_interval == 0)) fprintf(stderr, "[%s] Number of records processed: %lu.\n", __func__, count);
+		if(UNLIKELY(++count % notification_interval == 0)) {
+			LOG_INFO("Number of records processed: %lu.\n", count);
+		}
 	}
 	bam_destroy1(b);
 	bam_hdr_destroy(header);
 	sam_close(fp);
 	bed_destroy_hash(bed);
-	free(bedpath);
+	cond_free(bedpath);
 	fprintf(stdout, "Fraction of raw reads on target: %f. \nTotal raw reads: %lu. Raw reads on target: %lu.\n",
 			(double)fm_target / total_fm, total_fm, fm_target);
 	return EXIT_SUCCESS;
@@ -155,15 +179,9 @@ static void print_stats(famstats_t *stats, FILE *fp)
 	print_hashstats(stats, fp);
 }
 
-static inline void tag_test(const uint8_t *data, const char *tag)
-{
-	if(UNLIKELY(!data))
-		fprintf(stderr, "[E:%s] Required bam tag '%s' not found. Abort mission!\n", __func__, tag),
-		exit(EXIT_FAILURE);
-}
 
 
-static inline void famstat_loop(famstats_t *s, bam1_t *b, famstat_settings_t *settings)
+static inline void famstats_fm_loop(famstats_t *s, bam1_t *b, famstat_settings_t *settings)
 {
 	//tag_test(data, "FM");
 	if((b->core.flag & (BAM_FSECONDARY | BAM_FSUPPLEMENTARY | BAM_FQCFAIL | BAM_FREAD2)) ||
@@ -179,7 +197,7 @@ static inline void famstat_loop(famstats_t *s, bam1_t *b, famstat_settings_t *se
 	if(FM > 1) ++s->realfm_counts, s->realfm_sum += FM, s->realrc_sum += RV;
 	++s->allfm_counts, s->allfm_sum += FM, s->allrc_sum += RV;
 
-	LOG_DEBUG("RV value: %i. allrc_sum: %lu. realrc_sum: %lu.\n", RV, s->allrc_sum, s->realrc_sum);
+	//LOG_DEBUG("RV value: %i. allrc_sum: %lu. realrc_sum: %lu.\n", RV, s->allrc_sum, s->realrc_sum);
 
 	if((s->ki = kh_get(fm, s->fm, FM)) == kh_end(s->fm))
 		s->ki = kh_put(fm, s->fm, FM, &s->khr), kh_val(s->fm, s->ki) = 1;
@@ -214,12 +232,12 @@ famstats_t *famstat_core(samFile *fp, bam_hdr_t *h, famstat_settings_t *settings
 		LOG_ERROR("Could not read from input bam %s. Abort!\n", fp->fn);
 	}
 	++count;
-	famstat_loop(s, b, settings);
+	famstats_fm_loop(s, b, settings);
 	check_bam_tag(b, "FP");
 	check_bam_tag(b, "RV");
 	check_bam_tag(b, "FM");
 	while (LIKELY((ret = sam_read1(fp, h, b)) >= 0)) {
-		famstat_loop(s, b, settings);
+		famstats_fm_loop(s, b, settings);
 		if(UNLIKELY(++count % settings->notification_interval == 0))
 			LOG_INFO("Number of records processed: %lu.\n", count);
 	}
@@ -228,7 +246,7 @@ famstats_t *famstat_core(samFile *fp, bam_hdr_t *h, famstat_settings_t *settings
 	return s;
 }
 
-static int usage_exit(FILE *fp, int exit_status)
+static int famstats_usage_exit(FILE *fp, int exit_status)
 {
 	fprintf(fp, "Usage: bmftools famstats\n");
 	fprintf(fp, "Subcommands: \nfm\tFamily Size stats\n"
@@ -238,7 +256,7 @@ static int usage_exit(FILE *fp, int exit_status)
 	return exit_status;
 }
 
-static int fm_usage_exit(FILE *fp, int exit_status)
+static int famstats_fm_usage_exit(FILE *fp, int exit_status)
 {
 	fprintf(fp, "Usage: bmftools famstats fm <opts> <in.bam>\n");
 	fprintf(fp, "-m Set minimum mapping quality. Default: 0.\n");
@@ -247,7 +265,7 @@ static int fm_usage_exit(FILE *fp, int exit_status)
 	return exit_status;
 }
 
-int fm_main(int argc, char *argv[])
+int famstats_fm_main(int argc, char *argv[])
 {
 	samFile *fp;
 	bam_hdr_t *header;
@@ -269,13 +287,13 @@ int fm_main(int argc, char *argv[])
 		case 'n': settings->notification_interval = strtoull(optarg, NULL, 0); break;
 		case '?': // Fall-through!
 		case 'h':
-			return fm_usage_exit(stderr, EXIT_SUCCESS);
+			return famstats_fm_usage_exit(stderr, EXIT_SUCCESS);
 		}
 	}
 
 	if (argc != optind+1) {
-		if (argc == optind) fm_usage_exit(stdout, EXIT_SUCCESS);
-		else fm_usage_exit(stderr, EXIT_FAILURE);
+		if (argc == optind) famstats_fm_usage_exit(stdout, EXIT_SUCCESS);
+		else famstats_fm_usage_exit(stderr, EXIT_FAILURE);
 	}
 
 	LOG_INFO("Running main with minMQ %i and minFM %i.\n", settings->minMQ, settings->minFM);
@@ -300,7 +318,7 @@ int fm_main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
-static inline void frac_loop(bam1_t *b, int minFM, uint64_t *fm_above, uint64_t *fm_total)
+static inline void famstats_frac_loop(bam1_t *b, int minFM, uint64_t *fm_above, uint64_t *fm_total)
 {
 	if((b->core.flag & (BAM_FSECONDARY | BAM_FSUPPLEMENTARY | BAM_FQCFAIL | BAM_FREAD2)) ||
 			bam_aux2i(bam_aux_get(b, "FP")) == 0)
@@ -311,7 +329,7 @@ static inline void frac_loop(bam1_t *b, int minFM, uint64_t *fm_above, uint64_t 
 }
 
 
-int frac_main(int argc, char *argv[])
+int famstats_frac_main(int argc, char *argv[])
 {
 	samFile *fp;
 	bam_hdr_t *header;
@@ -319,8 +337,8 @@ int frac_main(int argc, char *argv[])
 	uint32_t minFM = 0;
 	uint64_t notification_interval = 1000000;
 
-	if(argc < 4) frac_usage_exit(stderr, EXIT_FAILURE);
-	if(strcmp(argv[1], "--help") == 0) frac_usage_exit(stderr, EXIT_SUCCESS);
+	if(argc < 4) famstats_frac_usage_exit(stderr, EXIT_FAILURE);
+	if(strcmp(argv[1], "--help") == 0) famstats_frac_usage_exit(stderr, EXIT_SUCCESS);
 
 	while ((c = getopt(argc, argv, "n:m:h?")) >= 0) {
 		switch (c) {
@@ -331,20 +349,20 @@ int frac_main(int argc, char *argv[])
 			notification_interval = strtoull(optarg, NULL, 0); break;
 		case '?':
 		case 'h':
-			return frac_usage_exit(stderr, EXIT_SUCCESS);
+			return famstats_frac_usage_exit(stderr, EXIT_SUCCESS);
 		default:
-			return frac_usage_exit(stderr, EXIT_FAILURE);
+			return famstats_frac_usage_exit(stderr, EXIT_FAILURE);
 		}
 	}
 
 	if(!minFM) {
-		LOG_ERROR("minFM not set. frac_main meaningless without it. Result: 1.0.\n");
+		LOG_ERROR("minFM not set. famstats_frac_main meaningless without it. Result: 1.0.\n");
 	}
 	LOG_INFO("Running frac main minFM %i.\n", minFM);
 
 	if (argc != optind+1) {
-		if (argc == optind) frac_usage_exit(stdout, EXIT_SUCCESS);
-		else frac_usage_exit(stderr, EXIT_FAILURE);
+		if (argc == optind) famstats_frac_usage_exit(stdout, EXIT_SUCCESS);
+		else famstats_frac_usage_exit(stderr, EXIT_FAILURE);
 	}
 	fp = sam_open(argv[optind], "r");
 	if (!fp) {
@@ -365,9 +383,9 @@ int frac_main(int argc, char *argv[])
 	check_bam_tag(b, "RV");
 	check_bam_tag(b, "FM");
 	check_bam_tag(b, "FA");
-	frac_loop(b, minFM, &fm_above, &total_fm), ++count;
+	famstats_frac_loop(b, minFM, &fm_above, &total_fm), ++count;
 	while (LIKELY(c = sam_read1(fp, header, b)) >= 0) {
-		frac_loop(b, minFM, &fm_above, &total_fm);
+		famstats_frac_loop(b, minFM, &fm_above, &total_fm);
 		if(UNLIKELY(!(++count % notification_interval)))
 			fprintf(stderr, "[%s] Number of records processed: %lu.\n", __func__, count);
 	}
@@ -381,16 +399,16 @@ int frac_main(int argc, char *argv[])
 int famstats_main(int argc, char *argv[])
 {
 	if(argc < 2)
-		return usage_exit(stderr, EXIT_FAILURE);
+		return famstats_usage_exit(stderr, EXIT_FAILURE);
 	if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
-		return usage_exit(stderr, EXIT_SUCCESS);
+		return famstats_usage_exit(stderr, EXIT_SUCCESS);
 	if(strcmp(argv[1], "fm") == 0)
-		return fm_main(argc - 1, argv + 1);
+		return famstats_fm_main(argc - 1, argv + 1);
 	if(strcmp(argv[1], "frac") == 0)
-		return frac_main(argc - 1, argv + 1);
+		return famstats_frac_main(argc - 1, argv + 1);
 	if(strcmp(argv[1], "target") == 0)
-		return target_main(argc - 1, argv + 1);
+		return famstats_target_main(argc - 1, argv + 1);
 	fprintf(stderr, "[E:%s] Unrecognized subcommand '%s'. See usage.\n", __func__, argv[1]);
-	return usage_exit(stderr, EXIT_FAILURE);
+	return famstats_usage_exit(stderr, EXIT_FAILURE);
 	return EXIT_FAILURE; // This never happens
 }
