eb674fb01ece10f50a1daf4ce632a8323670bda8
dnephi <daniel.baker@aruplab.com>
2016-01-28 11:40:29 -0700

    Added homopolymer pass/fail check to unit tests.

--- test/marksplit/marksplit_test.py
+++ test/marksplit/marksplit_test.py
@@ -5,6 +5,17 @@ import shlex
 import pysam
 import numpy as np
 
+def freq(collection):
+    ret = {}
+    for item in collection:
+        if item in ret:
+            ret[item] += 1
+        else:
+            ret[item] = 1
+    return ret
+
+mm_threshold = 12
+
 def get_tags(read):
     tag_strs = read.comment.split("\t")[1:]
     ret = {}
@@ -23,13 +34,16 @@ def check_bc(read):
     bc = read.comment.split("=")[2]
     fp = int(read.comment.split("|")[1].split("=")[1])
     assert bc[0] in "ZFR"
-    if "N" in bc:
+    freqs = freq(bc)
+    if "N" in freqs:
+        assert fp == 0
+    if max(freqs.values()) >= mm_threshold:
         assert fp == 0
 
 def marksplit_main():
     for ex in ["bmftools_db", "bmftools", "bmftools_p"]:
-        cstr = ("../../%s dmp -n0 -sTGACT -t12 -o marksplit_test_tmp -l 10 "
-                "-v 11 marksplit_test.R1.fq marksplit_test.R2.fq" % ex)
+        cstr = ("../../%s dmp -n0 -sTGACT -t%i -o marksplit_test_tmp -l 10 "
+                "-v 11 marksplit_test.R1.fq marksplit_test.R2.fq" % (ex, mm_threshold))
         print "Command string: %s" % cstr
         subprocess.check_call(shlex.split(cstr))
         for read in pysam.FastqFile("marksplit_test_tmp.tmp.0.R1.fastq"):
