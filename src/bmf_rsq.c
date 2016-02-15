874349fa27866ceb39b4bd6619a6bc2b8e4e188b
dnephi <daniel.baker@aruplab.com>
2016-02-14 20:41:36 -0700

    Style

--- src/bmf_rsq.c
+++ src/bmf_rsq.c
@@ -5,9 +5,7 @@ void resize_stack(tmp_stack_t *stack, size_t n) {
 	if(n > stack->max) {
 		stack->max = n;
 		stack->a = (bam1_t **)realloc(stack->a, sizeof(bam1_t *) * n);
-		if(!stack->a) {
-			LOG_ERROR("Failed to reallocate memory for %lu bam1_t * objects. Abort!\n", stack->max);
-		}
+		if(!stack->a) LOG_ERROR("Failed to reallocate memory for %lu bam1_t * objects. Abort!\n", stack->max);
 	} else if(n < stack->n){
 		for(uint64_t i = stack->n;i > n;) bam_destroy1(stack->a[--i]);
 		stack->max = n;
