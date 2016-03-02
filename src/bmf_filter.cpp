be982172ab8273c675abf37ba9a8c02eeaca0dbc
dnephi <daniel.baker@aruplab.com>
2016-03-01 19:41:16 -0700

    Expanded bmftools filter.

--- src/bmf_filter.cpp
+++ src/bmf_filter.cpp
@@ -5,7 +5,16 @@
 
 int usage(char **argv, int retcode=EXIT_FAILURE) {
     fprintf(stderr, "bmftools %s <-l output_compression_level> in.bam out.bam\n"
-                    "Use - for stdin or stdout.\n", argv[0]);
+                    "Use - for stdin or stdout.\n"
+                    "Flags-\n"
+                    "-F\t\tSkip all reads with any bits in parameter set.\n"
+                    "-f\t\tSkip reads sharing no bits in parameter set.\n"
+                    "-b\t\tPath to bed file with which to filter.\n"
+                    "-P\t\tNumber of bases around bed file regions to pad.\n"
+                    "-s\t\tMinimum family size for inclusion.\n"
+                    "-r\t\tIf set, writes failed reads to this file.\n"
+                    "-v\t\tInvert pass/fail, analogous to grep.\n"
+            , argv[0]);
     return retcode;
 }
 
@@ -18,57 +27,69 @@ struct opts {
     khash_t(bed) *bed;
 };
 
-#define TEST(b, options) \
+#define TEST(b, data, options) \
         (\
-             bam_itag(b, "FM") < (int)((opts *)options)->minFM ||\
-             b->core.qual < ((opts *)options)->minMQ ||\
-             (b->core.flag & ((opts *)options)->skip_flag) ||\
-             ((b->core.flag & ((opts *)options)->require_flag) == 0) ||\
-             !bed_test(b, ((opts *)options)->bed)\
+             (((data = bam_aux_get(b, "FM")) != NULL) ? bam_aux2i(data) : 1 >= (int)((opts *)options)->minFM) &&\
+             b->core.qual >= ((opts *)options)->minMQ &&\
+             ((b->core.flag & ((opts *)options)->skip_flag) == 0) &&\
+             ((b->core.flag & ((opts *)options)->require_flag)) &&\
+             bed_test(b, ((opts *)options)->bed)\
         )
 
 int bam_test(bam1_t *b, void *options) {
-    return ((opts *)options)->v ? !TEST(b, options): TEST(b, options);
+    uint8_t *data;
+    return ((opts *)options)->v ? !TEST(b, data, options): TEST(b, data, options);
 }
 
 #undef TEST
 
 int filter_main(int argc, char *argv[]) {
-    if(argc < 3) {
+    if(argc < 3)
         return usage(argv);
-    }
-    if(strcmp(argv[1], "--help") == 0) {
+    if(strcmp(argv[1], "--help") == 0)
         return usage(argv, EXIT_SUCCESS);
-    }
     int c;
     char out_mode[4] = "wb";
     opts param = {0};
     char *bedpath = NULL;
-    while((c = getopt(argc, argv, "b:m:F:f:l:hv?")) > -1) {
+    int padding = DEFAULT_PADDING;
+    std::string refused_path("");
+    while((c = getopt(argc, argv, "r:P:b:m:F:f:l:hv?")) > -1) {
         switch(c) {
+        case 'P': padding = atoi(optarg); break;
         case 'b': bedpath = optarg; break;
         case 'm': param.minMQ = strtoul(optarg, NULL, 0); break;
+        case 's': param.minFM = strtoul(optarg, NULL, 0); break;
         case 'F': param.skip_flag = strtoul(optarg, NULL, 0); break;
         case 'f': param.require_flag = strtoul(optarg, NULL, 0); break;
         case 'v': param.v = 1; break;
+        case 'r': refused_path = optarg; break;
         case 'l': out_mode[2] = atoi(optarg) % 10 + '0'; break;
         case 'h': case '?': return usage(argv, EXIT_SUCCESS);
         }
     }
+    check_bam_tag_exit(argv[optind], "FM");
     if(argc - 2 != optind) {
         LOG_EXIT("Required: precisely two positional arguments (in bam, out bam).\n");
     }
+    dlib::BamHandle in(argv[optind]);
     if(bedpath) {
-        // This doesn't properly handle streaming....
-        samFile *fp = sam_open(argv[optind], "r");
-        bam_hdr_t *hdr = sam_hdr_read(fp);
-        sam_close(fp);
-        param.bed = parse_bed_hash(bedpath, hdr, 0);
-        bam_hdr_destroy(hdr);
+        param.bed = parse_bed_hash(bedpath, in.header, padding);
     }
-    // Actually this function. You can't really apply a null function....
-    // Actually create your type for data and then provide it if needed.
-    dlib::bam_apply_function(argv[optind], argv[optind + 1], bam_test, (void *)&param, out_mode);
+    dlib::BamHandle out(argv[optind] + 1, in.header, out_mode);
+    // Core
+    if(!refused_path.size()) {
+        in.for_each(bam_test, out, (void *)&param);
+    } else {
+        dlib::BamHandle refused(refused_path.c_str(), in.header, out_mode);
+        while(in.next() >= 0) {
+            if(bam_test(in.rec, (void *)&param))
+                out.write(in.rec);
+            else
+                refused.write(in.rec);
+        }
+    }
+    // Clean up.
     if(param.bed) bed_destroy_hash((void *)param.bed);
     return EXIT_SUCCESS;
 }
