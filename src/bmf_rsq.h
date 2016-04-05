671f482a5cde074794ce82edd538496283aed02a
dnephi <daniel.baker@aruplab.com>
2016-04-05 12:33:03 -0600

    Expand bmftools rsq to use the mate's length while grouping.

--- src/bmf_rsq.h
+++ src/bmf_rsq.h
@@ -29,18 +29,21 @@ namespace BMF {
 
     CONST static inline int same_stack_pos_se(bam1_t *b, bam1_t *p)
     {
-        return bmfsort_se_key(b) == bmfsort_se_key(p);
+        return bmfsort_se_key(b) == bmfsort_se_key(p) &&
+                b->core.l_qseq == b->core.l_qseq;
     }
 
     CONST static inline int same_stack_ucs_se(bam1_t *b, bam1_t *p)
     {
-        return ucs_se_sort_key(b) == ucs_se_sort_key(p);
+        return ucs_se_sort_key(b) == ucs_se_sort_key(p)  &&
+                b->core.l_qseq == b->core.l_qseq;
     }
 
     CONST static inline int same_stack_pos(bam1_t *b, bam1_t *p)
     {
         return (bmfsort_core_key(b) == bmfsort_core_key(p) &&
-                bmfsort_mate_key(b) == bmfsort_mate_key(p));
+                bmfsort_mate_key(b) == bmfsort_mate_key(p) &&
+                sort_rlen_key(b) == sort_rlen_key(p));
     }
 
     CONST static inline int same_stack_ucs(bam1_t *b, bam1_t *p)
@@ -58,10 +61,10 @@ namespace BMF {
         return 0;
     #else
         return (ucs_sort_core_key(b) == ucs_sort_core_key(p) &&
-                ucs_sort_mate_key(b) == ucs_sort_mate_key(p));
+                ucs_sort_mate_key(b) == ucs_sort_mate_key(p) &&
+                sort_rlen_key(b) == sort_rlen_key(p));
     #endif
     }
-
 
     CONST static inline int read_pass_hd(bam1_t *b, bam1_t *p, const int lim=READ_HD_LIMIT)
     {
