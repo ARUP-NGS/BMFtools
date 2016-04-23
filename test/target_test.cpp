872b8bc1a8ac67fbcc9f3890a3efdc17433a445b
dnephi <daniel.baker@aruplab.com>
2016-04-22 18:21:16 -0600

    Update target unit test for precisely correct on-target calculations.

--- test/target_test.cpp
+++ test/target_test.cpp
@@ -3,12 +3,14 @@
 #include <stdint.h>
 //#include "htslib/sam.h"
 #include "src/bmf_target.h"
+#include "dlib/logging_util.h"
 
 int main(int c, char **argv)
 {
     BMF::target_counts_t counts = BMF::target_core((char *)"test/target_test.bed", (char *)"test/target_test.bam", 0u, 0u, 1000000);
     assert(counts.count == 2185uL);
     assert(counts.n_skipped == 0uL);
-    assert(counts.target == 2032uL);
+    if(counts.target != 2006uL)
+        LOG_EXIT("counts.target %lu rather than expected.\n", counts.target, 2006uL);
     return 0;
 }
