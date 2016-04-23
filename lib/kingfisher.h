6a6acc7b1f3038af138dcc402abaa00e04660585
dnephi <daniel.baker@aruplab.com>
2016-04-23 00:38:02 -0600

    Fixed a middle case.

--- lib/kingfisher.h
+++ lib/kingfisher.h
@@ -117,7 +117,8 @@ namespace BMF {
         arr += index * 5;
         return (arr[0] > arr[1]) ? ((arr[0] > arr[2]) ? ((arr[0] > arr[3]) ? (arr[0] > arr[4] ? 0: 4)
                                                                            : (arr[3] > arr[4] ? 3: 4))
-                                                      : (arr[2] > arr[4] ? 2: 4))
+                                                      : (arr[2] > arr[3])  ? (arr[2] > arr[4] ? 2: 4)
+                                                                           : (arr[3] > arr[4] ? 3: 4))
                                  : ((arr[1] > arr[2]) ? ((arr[1] > arr[3]) ? (arr[1] > arr[4] ? 1: 4)
                                                                            : (arr[3] > arr[4] ? 3: 4))
                                                       : ((arr[2] > arr[3]) ? (arr[2] > arr[4] ? 2: 4)
